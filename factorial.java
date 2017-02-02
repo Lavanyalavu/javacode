aff6ab1c7f2cb242eb32357793995ca7fcedb591
Lavanyalavu <lavu1995@gmail.com>
2017-02-02 14:56:45 +0530

    is this right

---
+++ factorial.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+class factorial
+{
+	public static void main(String arg[])
+	{
+		int i,fact=1,n=5;
+		for(i=1;i<=5;i++)
+		{
+			fact=fact*i;
+			System.out.println(+fact);
+		}
+		
+	}
+}
